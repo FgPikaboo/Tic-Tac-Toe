0833894e4d55b5e286cfc9ea5e601822c87e9ca6
FgPikaboo <pikaboosimulator@gmail.com>
2026-09-10 14:00:16 +0200

    Maj bot

--- bot_discord.mjs
+++ bot_discord.mjs
@@ -25,45 +25,29 @@ export class Bot_Discord extends EventEmitter {
 	eventListen() {
 		this._client.once('ready', () => {
 			console.log('Je suis connecté')
-			console.error(new Date().toISOString(),'Je suis connecté')
-			console.error(new Date().toISOString(),this.readyBot + ' Avant readyBot')
 			this.readyBot = true
-			console.error(new Date().toISOString(),this.readyBot + ' Apres readyBot')
 			this.emit('bot:ready')
 		})
 
 		// Quand un message est envoyé
 		this._client.on('messageCreate', (message) => {
-			console.error(new Date().toISOString(), 'Condition check ?')
 		// Si c'est notre propre message, on ne fait rien, cela évite le spam à l'infini (et l'au dela)
 			if (message.author.id === this._client.user.id) return
-			console.error(new Date().toISOString(), message + ' Valeur de messageCreate')
 			this.lastMessage = message
-			console.error(new Date().toISOString(), 'Avant envoyerMessage')
 			this.envoyerMessage(message)
-			console.error(new Date().toISOString(), 'Apres envoyerMessage')
 		})
 	}
 
 	initBot() {
 		const { token } = configToken
 
-		console.error(new Date().toISOString(), 'Avant eventListen')
 		this.eventListen()
-		console.error(new Date().toISOString(), 'Apres eventListen')
 
 		// Login à Discord avec le token du bot
-		console.error(new Date().toISOString(), 'Avant clientLogin')
 		this._client.login(token)
-		console.error(new Date().toISOString(), 'Apres clientLogin')
-
-		
-	
 	}
 
 	envoyerMessage(message) {
-		console.error(new Date().toISOString(), this.lastMessage + ' Valeur de last message')
-		console.error(new Date().toISOString(), message + ' Valeur de message')
 		return this.lastMessage && this.lastMessage.channel.send(message)
 	}
 
--- tictactoe/test_discord.mjs
+++ tictactoe/test_discord.mjs
@@ -7,9 +7,7 @@ export class Test_Discord {
 	}
 
 	launcherBot() {
-		console.error(new Date().toISOString(), 'Avant initBot')
 		this.botDiscord.initBot()
-		console.error(new Date().toISOString(), 'Apres initBot')
 
 		this.botDiscord.once('bot:ready', () => {
 			console.log('Je suis pret a envoyer un message') 
@@ -25,13 +23,5 @@ export class Test_Discord {
 	}
 }
 
-
-
-
-
-console.error(new Date().toISOString(), 'Avant T')
 const t = new Test_Discord()
-console.error(new Date().toISOString(), 'Apres T')
-console.error(new Date().toISOString(), 'Avant T.launch')
 t.launcherBot()
-console.error(new Date().toISOString(), 'Apres T.launch')
